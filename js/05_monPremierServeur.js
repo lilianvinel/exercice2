71cf408340a5546db073d367a879ffe6907fe331
Lilian Vinel <e1565626@cmaisonneuve.qc.ca>
2018-02-01 11:41:08 -0500

    changer le lien du local host à 3000

--- js/05_monPremierServeur.js
+++ js/05_monPremierServeur.js
@@ -5,4 +5,4 @@ http.createServer((request, response) =>
  response.writeHead(200, {"Content-Type": "text/plain"});
  response.write("Hello World"); 
  response.end(); 
-}).listen(8888);
+}).listen(3000);
